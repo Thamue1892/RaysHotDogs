debe98080a10eacaa7d8e8cabea30692a16dca81
Thamsanqa Mpofu <thamue1892@gmail.com>
2018-04-19 10:41:20 +0200

    create the HotDogDetailView.axml

---
+++ RaysHotDogs/HotDogDetailActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RaysHotDogs.Core.Model;
+using RaysHotDogs.Core.Service;
+
+namespace RaysHotDogs
+{
+    [Activity(Label = "Hot DogDetailActivity")]
+    public class HotDogDetailActivity : Activity
+    {
+        private ImageView hotDogImageView;
+        private TextView hotDogNameTextView;
+        private TextView shortDescriptionTextView;
+        private TextView descriptionTextView;
+        private TextView priceTextView;
+        private TextView amounTextView;
+        private EditText amountEditText;
+        private Button cancelButton;
+        private Button orderButton;
+
+        private HotDog selecteddHotDog;
+        private HotDogDataService dataService;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.HotDogDetailView);
+
+            HotDogDataService dataService = new HotDogDataService();
+            selecteddHotDog = dataService.GetHotDogById(1);
+
+            // Create your application here
+        }
+
+        private void FindViews()
+        {
+            hotDogImageView = FindViewById<ImageView>(Resource.Id.hotDogImageView);
+        }
+    }
+}
