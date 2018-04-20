9ad5dce554cb6feead9023d82950400845cc756e
Thamsanqa Mpofu <thamue1892@gmail.com>
2018-04-20 15:57:57 +0200

    Added navigation to the app

--- RaysHotDogs/HotDogDetailActivity.cs
+++ RaysHotDogs/HotDogDetailActivity.cs
@@ -5,6 +5,7 @@ using RaysHotDogs.Core.Model;
 using RaysHotDogs.Core.Service;
 using RaysHotDogs.Utility;
 using System;
+using Android.Content;
 
 namespace RaysHotDogs
 {
@@ -31,7 +32,9 @@ namespace RaysHotDogs
             SetContentView(Resource.Layout.HotDogDetailView);
 
             HotDogDataService dataService = new HotDogDataService();
-            selectedHotDog = dataService.GetHotDogById(1);
+
+            var selectedHotDogId = Intent.Extras.GetInt("selectedHotDogId");
+            selectedHotDog = dataService.GetHotDogById(selectedHotDogId);
 
             FindViews();
             BindData();
@@ -77,10 +80,13 @@ namespace RaysHotDogs
         {
             var amount = Int32.Parse(amountEditText.Text);
 
-            var dialog = new AlertDialog.Builder(this);
-            dialog.SetTitle("Confirmation");
-            dialog.SetMessage("Your hot dog has been added to your cart!");
-            dialog.Show();
+            var intent = new Intent();
+            intent.PutExtra("selectedHotDogId", selectedHotDog.HotDogId);
+            intent.PutExtra("amount", amount);
+
+            SetResult(Result.Ok,intent);
+
+            this.Finish();
         }
     }
 }
--- RaysHotDogs/HotDogMenuActivity.cs
+++ RaysHotDogs/HotDogMenuActivity.cs
@@ -5,10 +5,12 @@ using RaysHotDogs.Adapters;
 using RaysHotDogs.Core.Model;
 using RaysHotDogs.Core.Service;
 using System.Collections.Generic;
+using Android.Content;
+using Java.Lang;
 
 namespace RaysHotDogs
 {
-    [Activity(Label = "Hot Dog Menu", MainLauncher = true)]
+    [Activity(Label = "Hot Dog Menu")]
     public class HotDogMenuActivity : Activity
     {
         private ListView hotDogListView;
@@ -31,6 +33,33 @@ namespace RaysHotDogs
             hotDogListView.Adapter = new HotDogListAdapter(this, allHotDogs);
 
             hotDogListView.FastScrollEnabled = true;
+
+            hotDogListView.ItemClick += HotDogListView_ItemClick;
+        }
+
+        private void HotDogListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var hotDog = allHotDogs[e.Position];
+            var intent = new Intent();
+            intent.SetClass(this, typeof(HotDogDetailActivity));
+            intent.PutExtra("selectedHotDogId", hotDog.HotDogId);
+
+            StartActivityForResult(intent,100);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (resultCode == Result.Ok && requestCode == 100)
+            {
+                var selectedHotDog = hotDogDataService.GetHotDogById(data.GetIntExtra("selectedHotDogId", 0));
+
+                var dialog = new AlertDialog.Builder(this);
+                dialog.SetTitle("Confirmation");
+                dialog.SetMessage(string.Format("You've added {0} time(s) the {1}", data.GetIntExtra("amount", 0), selectedHotDog.Name));
+                dialog.Show();
+            }
         }
     }
 }
